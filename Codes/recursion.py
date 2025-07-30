4e5eb7257a43f882cb3f9f2eaf80ea46aec3c2dd
Aqeel Ahmed <100577515+aqeel2022@users.noreply.github.com>
2025-07-30 13:42:57 +0200

    web crawling using recursion

--- Codes/recursion.py
+++ Codes/recursion.py
@@ -17,16 +17,61 @@ A condition that stops the recursion to prevent infinite loops.
 #factorial using recursion
 
 
-def factorial(n):
-    #Base Case
+# def factorial(n):
+#     #Base Case
+#
+#     if n==0:
+#         return 1
+#
+#     #Recursive case
+#
+#     else:
+#         return  n * factorial(n-1)
+#
+#
+# print(factorial(10))
 
-    if n==0:
-        return 1
-
-    #Recursive case
-
-    else:
-        return  n * factorial(n-1)
 
 
-print(factorial(10))
+#fibonacci series
+
+# def fibonacci(n):
+#     if n==0:
+#         return 0
+#
+#     elif n==1:
+#         return 1
+#
+#     else:
+#         return fibonacci(n-1)+fibonacci(n-2)
+#
+# print(fibonacci(6))
+
+
+import requests
+from bs4 import BeautifulSoup
+
+def crawl(url, depth):
+    if depth == 0:
+        return
+
+    try:
+        response=requests.get(url)
+        soup = BeautifulSoup(response.text ,"html.parser")
+
+        print("Visited:", url)
+
+        for link in soup.find_all("a", href=True):
+            new_url = link['href']
+
+            if new_url.startswith("http"):
+                crawl(new_url, depth-1)
+
+
+
+    except:
+        pass
+
+
+
+crawl("https://www.perplexity.ai/search/what-is-the-contribution-for-f-8Sn_JfqXRTuBnTXlShr3KA", 2)
